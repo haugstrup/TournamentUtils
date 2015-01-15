bb2f53c891ce5432ddedad4ef1b2ff913ce504f0
Andreas Haugstrup Pedersen <andreas@solitude.dk>
2015-01-14 20:05:12 -0800

    Get rid og debug data

--- src/HeadToHeadPairing.php
+++ src/HeadToHeadPairing.php
@@ -76,23 +76,3 @@ class HeadToHeadPairing {
   }
 
 }
-
-$groups = array(
-  array(
-    'Andreas' => array('Per' => 1, 'Darren' => 1),
-    'Per' => array('Matt' => 1, 'Andreas' => 1),
-    'Shon' => array('Sally' => 1, 'Eric' => 1)
-  ),
-  array(
-    'Sally' => array('Darren' => 1, 'Shon' => 1),
-    'Darren' => array('Andreas' => 1, 'Sally' => 1),
-    'Matt' => array('Per' => 1, 'Eric' => 1),
-    'Eric' => array('Shon' => 1, 'Matt' => 1)
-  )
-);
-
-$builder = new HeadToHeadPairing($groups);
-$pairings = $builder->build();
-
-print_r($pairings);
-
