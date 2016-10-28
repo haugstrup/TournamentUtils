abe97ba795d2ebe18b52c81d4ba44a91371f54a2
Andreas Haugstrup Pedersen <andreas@solitude.dk>
2016-10-27 17:06:33 -0700

    Add everything to autoloader

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,3 +1,14 @@
 <?php
 
 require_once 'src/AdjacentPairing.php';
+require_once 'src/ArenaSelector.php';
+require_once 'src/BalancedArena.php';
+require_once 'src/BalancedPairing.php';
+require_once 'src/BalancedPlayerOrder.php';
+require_once 'src/CrossPairing.php';
+require_once 'src/GroupTieredSwissPairing.php';
+require_once 'src/HeadToHeadSwissPairing.php';
+require_once 'src/RandomOptimizer.php';
+require_once 'src/RoundRobinPairing.php';
+require_once 'src/SingleEliminationBracket.php';
+require_once 'src/SlaughterPairing.php';
