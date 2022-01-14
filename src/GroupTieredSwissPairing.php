e582e521525d9c25bf2b39dab1f5ca822c1e7695
Andreas Haugstrup Pedersen <andreas@solitude.dk>
2022-01-13 23:52:15 -0800

    Add 8 rounds tiered swiss pairings

--- src/GroupTieredSwissPairing.php
+++ src/GroupTieredSwissPairing.php
@@ -2,30 +2,32 @@
 
 require_once 'Base.php';
 
-class GroupTieredSwissPairing extends Base {
+class GroupTieredSwissPairing extends Base
+{
+    public $rounds = 5;
+    public $players = array();
+    public $group_maps = array();
+    public $max_players = 128;
+    protected $min_players = 16;
 
-  public $rounds = 5;
-  public $players = array();
-  public $group_maps = array();
-  public $max_players = 128;
-  protected $min_players = 16;
+    public function __construct($rounds, $players, $options = array())
+    {
+        $this->players = $players;
+        $this->rounds = (int)$rounds;
+        if (isset($options['max_players'])) {
+            $this->max_players = $options['max_players'];
+        }
 
-  public function __construct($rounds, $players, $options = array()) {
-    $this->players = $players;
-    $this->rounds = (int)$rounds;
-    if (isset($options['max_players'])) {
-      $this->max_players = $options['max_players'];
+        $this->init_group_maps();
+
+        if (count($this->players) < $this->min_players) {
+            throw new \Exception('You must have at least 16 players');
+        }
     }
 
-    $this->init_group_maps();
-
-    if (count($this->players) < $this->min_players) {
-      throw new \Exception('You must have at least 16 players');
-    }
-  }
-
-  public function init_group_maps() {
-    $maps = array(
+    public function init_group_maps()
+    {
+        $maps = array(
       5 => array(
         16 => array(16, 16, 8, 8, 4),
         20 => array(20, array(12, 8), array(12, 8), array(8, 8, 4), 4),
@@ -103,6 +105,43 @@ class GroupTieredSwissPairing extends Base {
         124 => array(124, array(64, 60), array(64, 60),array(32, 32, 32, 28), array(16, 16, 16, 16, 16, 16, 16, 12), array(16, 16, 16, 16, 16, 16, 16, 12), 4),
         128 => array(128, 64, 64, 32, 16, 16, 4),
       ),
+
+      // 8 rounds, for IFPA WC
+      8 => [
+        16 => [16, 16, 16, 16, 8, 8, 8, 4],
+        20 => [20, 20, [12, 8], [12, 8], [12, 8], [8, 8, 4], [8, 8, 4], 4],
+        24 => [24, 24, 12, 12, 12, 8, 8, 4],
+        28 => [28, 28, [16, 12], [16, 12],[16, 12], [8, 8, 8, 4], [8, 8, 8, 4],4],
+        32 => [32, 32, 16, 16, 16, 8, 8, 4],
+        36 => [36, 36, [20, 16],[20, 16], 12, [8, 8, 8, 8, 4], [8, 8, 8, 8, 4],4],
+        40 => [40, 40, 20, 20,[16, 16, 8], 8, 8, 4],
+        44 => [44, 44, [24, 20],[24, 20], [16, 16, 12], [8, 8, 8, 8, 8, 4], [8, 8, 8, 8, 8, 4],4],
+        48 => [48, 48, 24, 24, 16, 8, 8, 4],
+        52 => [52, 52, [28, 24], [28, 24],[20, 20, 12], [8, 8, 8, 8, 8, 8, 4], [8, 8, 8, 8, 8, 8, 4],4],
+        56 => [56, 56, 28,28, [16, 16, 16, 8], 8, 8, 4],
+        60 => [60, 60, 20, 20, 12, [8, 8, 8, 8, 8, 8, 8, 4],[8, 8, 8, 8, 8, 8, 8, 4], 4],
+        64 => [64, 64, 32, 32, 16, 8, 8, 4],
+        68 => [68, 68, [36, 32], [36, 32],[20, 16, 16, 16], [12, 12, 12, 12, 12, 8], [12, 12, 12, 12, 12, 8],4],
+        72 => [72, 72, 36, 36, 24, 12, 12, 4],
+        76 => [76, 76, [40, 36], [40, 36],[20, 20, 20, 16], [16, 16, 16, 16, 12], [16, 16, 16, 16, 12],4],
+
+        // IFPA WC pairings, don't modify:
+        80 => [80, 80, 40, 40, 20, 16, 8, 4],
+        // End of IFPA WC pairings
+
+        84 => [84, 84, [44, 40], [44, 40], [24, 20, 20, 20], [16, 16, 16, 16, 12, 8], [16, 16, 16, 16, 12, 8],4],
+        88 => [88, 88, 44, 44, [24, 24, 20, 20], [16, 16, 16, 16, 16, 8], [16, 16, 16, 16, 16, 8],4],
+        92 => [92, 92, [48, 44], [48, 44],[24, 24, 24, 20], [16, 16, 16, 16, 16, 12], [16, 16, 16, 16, 16, 12],4],
+        96 => [96, 96, 48, 48, 24, 16, 16, 4],
+        100 => [100, 100, [52, 48], [52, 48],[28, 24, 24, 24], [16, 16, 16, 16, 16, 12, 8], [16, 16, 16, 16, 16, 12, 8],4],
+        104 => [104, 104, 52, 52, [28, 28, 24, 24], [16, 16, 16, 16, 16, 16, 8], [16, 16, 16, 16, 16, 16, 8],4],
+        108 => [108, 108, [56, 52], [56, 52],[28, 28, 28, 24], [16, 16, 16, 16, 16, 16, 12], [16, 16, 16, 16, 16, 16, 12],4],
+        112 => [112, 112, 56, 56, 28, 16, 16, 4],
+        116 => [116, 116, [60, 56], [60, 56],[32, 28, 28, 28], [16, 16, 16, 16, 16, 16, 12, 8],[16, 16, 16, 16, 16, 16, 12, 8], 4],
+        120 => [120, 120, 60, 60, [32, 32, 28, 28], [16, 16, 16, 16, 16, 16, 16, 8], [16, 16, 16, 16, 16, 16, 16, 8],4],
+        124 => [124, 124, [64, 60], [64, 60],[32, 32, 32, 28], [16, 16, 16, 16, 16, 16, 16, 12], [16, 16, 16, 16, 16, 16, 16, 12], 4],
+        128 => [128, 128, 64, 64, 32, 16, 16, 4],
+      ],
 
       10 => array(
         16 => array(16, 16, 16, 16, 8, 8, 8, 8, 8, 4),
@@ -215,138 +254,136 @@ class GroupTieredSwissPairing extends Base {
       // )
     );
 
-    // Prep group maps for 3 and 4 rounds
-    foreach ($maps[5] as $index => $tiers) {
+        // Prep group maps for 3 and 4 rounds
+        foreach ($maps[5] as $index => $tiers) {
 
       // For three rounds, remove index 1, 2
-      $three_round_tiers = $tiers;
-      array_splice($three_round_tiers, 1, 2);
+            $three_round_tiers = $tiers;
+            array_splice($three_round_tiers, 1, 2);
 
-      // For four rounds, remove index 2 (the middle round)
-      $four_round_tiers = $tiers;
-      array_splice($four_round_tiers, 2, 1);
+            // For four rounds, remove index 2 (the middle round)
+            $four_round_tiers = $tiers;
+            array_splice($four_round_tiers, 2, 1);
 
-      $maps[3][$index] = $three_round_tiers;
-      $maps[4][$index] = $four_round_tiers;
-    }
-
-    $this->group_maps = $maps;
-  }
-
-  public function get_group_map() {
-    $player_count = count($this->players);
-    if (isset($this->group_maps[$this->rounds][$player_count])) {
-      $map = $this->group_maps[$this->rounds][$player_count];
-      $key = $player_count;
-    }
-    else {
-      $i = $player_count;
-      while ($i <= $this->max_players) {
-        if (isset($this->group_maps[$this->rounds][$i])) {
-          $map = $this->group_maps[$this->rounds][$i];
-          $key = $i;
-          break;
+            $maps[3][$index] = $three_round_tiers;
+            $maps[4][$index] = $four_round_tiers;
         }
-        $i++;
-      }
+
+        $this->group_maps = $maps;
     }
 
-    if (!$map) {
-      throw new \Exception('Couldn\'t find tier map');
+    public function get_group_map()
+    {
+        $player_count = count($this->players);
+        if (isset($this->group_maps[$this->rounds][$player_count])) {
+            $map = $this->group_maps[$this->rounds][$player_count];
+            $key = $player_count;
+        } else {
+            $i = $player_count;
+            while ($i <= $this->max_players) {
+                if (isset($this->group_maps[$this->rounds][$i])) {
+                    $map = $this->group_maps[$this->rounds][$i];
+                    $key = $i;
+                    break;
+                }
+                $i++;
+            }
+        }
+
+        if (!$map) {
+            throw new \Exception('Couldn\'t find tier map');
+        }
+
+        return array('map' => $map, 'key' => $key);
     }
 
-    return array('map' => $map, 'key' => $key);
-  }
-
-  // Find the specific map for a specific round
-  public function get_round_map($round) {
-    $map = $this->get_group_map();
-    $round_map = $map['map'][$round];
-    if (is_int($round_map)) {
-      $round_map = array_pad(array(), $map['key']/$round_map, $round_map);
-    }
-    return $round_map;
-  }
-
-  // Build groups for a specific round
-  public function build($round) {
-    $players = $this->players;
-    $player_count = count($this->players);
-    $groups = array();
-
-    // Stay within boundaries
-    if ($round >= $this->rounds || $round < 0) {
-      throw new \Exception('Too many or too few rounds');
-    }
-    if ($player_count > $this->max_players || $player_count < 0) {
-      throw new \Exception('Too many or too few players');
+    // Find the specific map for a specific round
+    public function get_round_map($round)
+    {
+        $map = $this->get_group_map();
+        $round_map = $map['map'][$round];
+        if (is_int($round_map)) {
+            $round_map = array_pad(array(), $map['key']/$round_map, $round_map);
+        }
+        return $round_map;
     }
 
-    $round_map = $this->get_round_map($round);
+    // Build groups for a specific round
+    public function build($round)
+    {
+        $players = $this->players;
+        $player_count = count($this->players);
+        $groups = array();
 
-    foreach ($round_map as $index => $size) {
-      $number_of_players = $size;
+        // Stay within boundaries
+        if ($round >= $this->rounds || $round < 0) {
+            throw new \Exception('Too many or too few rounds');
+        }
+        if ($player_count > $this->max_players || $player_count < 0) {
+            throw new \Exception('Too many or too few players');
+        }
 
-      // If we're in the third to last group and...
-      // ...there are 9 players left
-      // ...all tiers are 4
-      // Then make all three player groups
-      if (count($round_map)-3 == $index && count($players) == 9 && $round_map[$index] === 4) {
-        $number_of_players--;
-      }
+        $round_map = $this->get_round_map($round);
+
+        foreach ($round_map as $index => $size) {
+            $number_of_players = $size;
+
+            // If we're in the third to last group and...
+            // ...there are 9 players left
+            // ...all tiers are 4
+            // Then make all three player groups
+            if (count($round_map)-3 == $index && count($players) == 9 && $round_map[$index] === 4) {
+                $number_of_players--;
+            }
 
 
-      // If we're in the second to last group and...
-      // ...we need to create 2 three-player groups
-      // ...or we need to create 3 three-player groups
-      // Then decrease the amount of players we grab for second-to-last group
-      if (count($round_map)-2 == $index) {
+            // If we're in the second to last group and...
+            // ...we need to create 2 three-player groups
+            // ...or we need to create 3 three-player groups
+            // Then decrease the amount of players we grab for second-to-last group
+            if (count($round_map)-2 == $index) {
 
         // Remaining players == required left players-2
-        if (count($players) === ($round_map[$index]+$round_map[$index+1]-2)) {
-          if ($round_map[$index+1] === 4) {
-            $number_of_players--;
-          }
+                if (count($players) === ($round_map[$index]+$round_map[$index+1]-2)) {
+                    if ($round_map[$index+1] === 4) {
+                        $number_of_players--;
+                    }
+                }
+
+                // Remaining players == required players-3
+                if (count($players) === ($round_map[$index]+$round_map[$index+1]-3)) {
+                    if ($round_map[$index+1] === 8) {
+                        $number_of_players--;
+                    }
+
+                    // Next round calls for 4 players
+                    if ($round_map[$index+1] === 4) {
+                        $number_of_players--;
+                        $number_of_players--;
+                    }
+                }
+            }
+
+            $tier_players = array_splice($players, 0, $number_of_players);
+
+            // Get people into groups.
+            // Nb. if there's only 6 players left, create two 3 player groups
+            $number_of_groups = $size/4;
+
+            for ($j=0;$j<$number_of_groups;$j++) {
+                $three_player_group = count($tier_players) == 9 || count($tier_players) == 6 || count($tier_players) == 3;
+                $middle_offset = ceil(count($tier_players)/2)-2;
+
+                // Grab the first, middle and last players
+                $group = array();
+                $group = array_merge($group, array_splice($tier_players, 0, 1));
+                $group = array_merge($group, array_splice($tier_players, $middle_offset, $three_player_group ? 1 : 2));
+                $group = array_merge($group, array_splice($tier_players, -1, 1));
+
+                $groups[] = $group;
+            }
         }
 
-        // Remaining players == required players-3
-        if (count($players) === ($round_map[$index]+$round_map[$index+1]-3)) {
-
-          if ($round_map[$index+1] === 8) {
-            $number_of_players--;
-          }
-
-          // Next round calls for 4 players
-          if ($round_map[$index+1] === 4) {
-            $number_of_players--;
-            $number_of_players--;
-          }
-        }
-
-      }
-
-      $tier_players = array_splice($players, 0, $number_of_players);
-
-      // Get people into groups.
-      // Nb. if there's only 6 players left, create two 3 player groups
-      $number_of_groups = $size/4;
-
-      for($j=0;$j<$number_of_groups;$j++) {
-        $three_player_group = count($tier_players) == 9 || count($tier_players) == 6 || count($tier_players) == 3;
-        $middle_offset = ceil(count($tier_players)/2)-2;
-
-        // Grab the first, middle and last players
-        $group = array();
-        $group = array_merge($group, array_splice($tier_players, 0, 1));
-        $group = array_merge($group, array_splice($tier_players, $middle_offset, $three_player_group ? 1 : 2));
-        $group = array_merge($group, array_splice($tier_players, -1, 1));
-
-        $groups[] = $group;
-      }
-
+        return $groups;
     }
-
-    return $groups;
-  }
-
 }
