ec0126cc5d43eeceafbd21863b9252e4f92343a1
Andreas Haugstrup Pedersen <andreas@solitude.dk>
2022-09-18 15:19:01 -0700

    Reformat code

--- src/BalancedArena.php
+++ src/BalancedArena.php
@@ -3,100 +3,109 @@
 require_once 'RandomOptimizer.php';
 
 class BalancedArena extends RandomOptimizer {
-  public $groups = array();
-  public $available_arenas = array();
-  public $arena_plays = array();
+    public $groups = [];
+    public $available_arenas = [];
+    public $arena_plays = [];
 
-  public function __construct($groups, $available_arenas, $amount = 1, $arena_plays = array()) {
-    $this->groups = $groups;
-    $this->available_arenas = $available_arenas;
-    $this->arena_plays = $arena_plays;
-    $this->amount = $amount;
-  }
-
-  public function solution($input) {
-    // For each group pick a random arena, only allow each arena to be used once
-    $solution = array();
-
-    // Shuffle groups so random groups get arena assigned
-    // when there are more groups than arenas
-    $group_keys = array_keys($input['groups']);
-    $group_keys = $this->shuffle($group_keys);
-    $input['available_arenas'] = $this->shuffle($input['available_arenas']);
-
-    $arenas = array();
-    for($i=0;$i<$this->amount;$i++) {
-      $arenas[$i] = $input['available_arenas'];
+    public function __construct(
+        $groups,
+        $available_arenas,
+        $amount = 1,
+        $arena_plays = []
+    ) {
+        $this->groups = $groups;
+        $this->available_arenas = $available_arenas;
+        $this->arena_plays = $arena_plays;
+        $this->amount = $amount;
     }
 
-    foreach($group_keys as $group_key) {
-      $group = $input['groups'][$group_key];
-      $chosen_arenas = array();
+    public function solution($input) {
+        // For each group pick a random arena, only allow each arena to be used once
+        $solution = [];
 
-      foreach ($arenas as $i => $list) {
-        $available = $list;
-        if (count($chosen_arenas) > 0) {
-          foreach ($available as $j => $a) {
-            if (in_array($a, $chosen_arenas)) {
-              unset($available[$j]);
-            }
-          }
+        // Shuffle groups so random groups get arena assigned
+        // when there are more groups than arenas
+        $group_keys = array_keys($input['groups']);
+        $group_keys = $this->shuffle($group_keys);
+        $input['available_arenas'] = $this->shuffle($input['available_arenas']);
+
+        $arenas = [];
+        for($i = 0; $i < $this->amount; $i++) {
+            $arenas[$i] = $input['available_arenas'];
         }
 
+        foreach($group_keys as $group_key) {
+            $group = $input['groups'][$group_key];
+            $chosen_arenas = [];
 
-        $chosen_arenas[$i] = null;
-        if (count($available) > 0) {
-          $key = $this->array_rand($available, 1);
-          $chosen_arenas[$i] = $available[$key];
+            foreach ($arenas as $i => $list) {
+                $available = $list;
+                if (count($chosen_arenas) > 0) {
+                    foreach ($available as $j => $a) {
+                        if (in_array($a, $chosen_arenas)) {
+                            unset($available[$j]);
+                        }
+                    }
+                }
 
-          // Unset from master list
-          foreach ($arenas[$i] as $j => $a) {
-            if ($a == $chosen_arenas[$i]) {
-              unset($arenas[$i][$j]);
-              break;
+                $chosen_arenas[$i] = null;
+                if (count($available) > 0) {
+                    $key = $this->array_rand($available, 1);
+                    $chosen_arenas[$i] = $available[$key];
+
+                    // Unset from master list
+                    foreach ($arenas[$i] as $j => $a) {
+                        if ($a == $chosen_arenas[$i]) {
+                            unset($arenas[$i][$j]);
+                            break;
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
 
-      $solution[] = array('group_index' => $group_key, 'group' => $group, 'arenas' => $chosen_arenas);
+            $solution[] = [
+                'group_index' => $group_key,
+                'group' => $group,
+                'arenas' => $chosen_arenas
+            ];
+        }
+
+        return $solution;
     }
 
-    return $solution;
-  }
+    public function cost($solution) {
+        // Cost is a function of how many plays the arena has gotten
+        $cost = 0;
 
-  public function cost($solution) {
-    // Cost is a function of how many plays the arena has gotten
-    $cost = 0;
+        foreach ($solution as $item) {
+            foreach ($item['group'] as $player_id) {
+                if (isset($this->arena_plays[$player_id])) {
+                    foreach ($item['arenas'] as $arena) {
+                        if (isset($this->arena_plays[$player_id][$arena])) {
+                            $cost = $cost+pow($this->arena_plays[$player_id][$arena], 2);
+                        }
+                    }
+                }
 
-    foreach ($solution as $item) {
-      foreach ($item['group'] as $player_id) {
-
-        if (isset($this->arena_plays[$player_id])) {
-          foreach ($item['arenas'] as $arena) {
-            if (isset($this->arena_plays[$player_id][$arena])) {
-              $cost = $cost+pow($this->arena_plays[$player_id][$arena], 2);
             }
-          }
         }
 
-      }
+        return $cost;
     }
 
-    return $cost;
-  }
+    public function build() {
+        $result = $this->solve([
+            'groups' => $this->groups,
+            'available_arenas' => $this->available_arenas
+        ]);
+        $groups = [];
 
-  public function build() {
-    $result = $this->solve(array('groups' => $this->groups, 'available_arenas' => $this->available_arenas));
-    $groups = array();
+        foreach ($result['solution'] as $matchup) {
+            $groups[$matchup['group_index']] = $matchup['arenas'];
+        }
 
-    foreach ($result['solution'] as $matchup) {
-      $groups[$matchup['group_index']] = $matchup['arenas'];
+        ksort($groups);
+
+        return ['cost' => $result['cost'], 'groups' => $groups];
     }
-
-    ksort($groups);
-
-    return array('cost' => $result['cost'], 'groups' => $groups);
-  }
-
 }
--- src/RandomOptimizer.php
+++ src/RandomOptimizer.php
@@ -3,38 +3,37 @@
 require_once 'Base.php';
 
 class RandomOptimizer extends Base {
-  public $iterations = 1000;
+    public $iterations = 1000;
 
-  // Implement this in your subclass
-  public function cost($solution) {
-    return null;
-  }
-
-  // Implement this in your subclass
-  public function solution($input) {
-    return null;
-  }
-
-  public function solve($input) {
-    $best_cost = null;
-    $best_solution = null;
-
-    for($i=0;$i<$this->iterations;$i++) {
-
-      $solution = $this->solution($input);
-      $cost = $this->cost($solution);
-
-      if (is_null($best_cost) || $cost < $best_cost) {
-        $best_cost = $cost;
-        $best_solution = $solution;
-
-        if ($cost == 0) {
-          break;
-        }
-      }
+    // Implement this in your subclass
+    public function cost($solution) {
+        return null;
     }
 
-    return array('cost' => $best_cost, 'solution' => $best_solution);
-  }
+    // Implement this in your subclass
+    public function solution($input) {
+        return null;
+    }
 
+    public function solve($input) {
+        $best_cost = null;
+        $best_solution = null;
+
+        for($i = 0; $i < $this->iterations; $i++) {
+
+            $solution = $this->solution($input);
+            $cost = $this->cost($solution);
+
+            if (is_null($best_cost) || $cost < $best_cost) {
+                $best_cost = $cost;
+                $best_solution = $solution;
+
+                if ($cost == 0) {
+                    break;
+                }
+            }
+        }
+
+        return array('cost' => $best_cost, 'solution' => $best_solution);
+    }
 }
