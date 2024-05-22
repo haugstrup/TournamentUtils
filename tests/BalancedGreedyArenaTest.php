42890b91e8e7e5922d8d3abb51d39308765da0cc
Andreas Haugstrup Pedersen <andreas@solitude.dk>
2024-05-21 23:09:49 -0700

    More tests

--- tests/BalancedGreedyArenaTest.php
+++ tests/BalancedGreedyArenaTest.php
@@ -22,6 +22,56 @@ class BalancedGreedyArenaTest extends TestCase {
         $this->assertEquals($solution['cost'], 0);
     }
 
+    public function testAssignsArenaOnlyOnce() {
+        $groups = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+        ];
+        $arenas = [90, 91];
+        $amount = 1;
+
+        $builder = new haugstrup\TournamentUtils\BalancedGreedyArena($groups, $arenas, $amount, []);
+        $solution = $builder->build();
+
+        $used_arenas = [];
+        foreach ($solution['groups'] as $group) {
+            $used_arenas[] = $group[0];
+        }
+        sort($used_arenas);
+        $this->assertEquals($used_arenas, [null, 90, 91]);
+
+        $this->assertEquals(count($solution['groups']), count($groups));
+        $this->assertEquals($solution['cost'], 0);
+    }
+
+    public function testAssignsArenaOncePerAmount() {
+        $groups = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+        ];
+        $arenas = [90, 91];
+        $amount = 2;
+
+        $builder = new haugstrup\TournamentUtils\BalancedGreedyArena($groups, $arenas, $amount, []);
+        $solution = $builder->build();
+
+        $first_used_arenas = [];
+        $second_used_arenas = [];
+        foreach ($solution['groups'] as $group) {
+            $first_used_arenas[] = $group[0];
+            $second_used_arenas[] = $group[1];
+        }
+        sort($first_used_arenas);
+        sort($second_used_arenas);
+        $this->assertEquals($first_used_arenas, [null, 90, 91]);
+        $this->assertEquals($second_used_arenas, [null, 90, 91]);
+
+        $this->assertEquals(count($solution['groups']), count($groups));
+        $this->assertEquals($solution['cost'], 0);
+    }
+
     public function testCalculatesCostWithNoPreviousPlays() {
         $arena_plays = [];
         $builder = new haugstrup\TournamentUtils\BalancedGreedyArena([], [], 1, $arena_plays);
