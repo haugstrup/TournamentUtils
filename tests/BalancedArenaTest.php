3798f95b1a0313ba2d1b9157413130d7d0a2f46b
Andreas Haugstrup Pedersen <andreas@solitude.dk>
2024-12-02 21:23:09 -0800

    Add test

--- tests/BalancedArenaTest.php
+++ tests/BalancedArenaTest.php
@@ -80,4 +80,39 @@ class BalancedArenaTest extends TestCase {
 		$this->assertNull($groups['groups'][3][0]);
 		$this->assertNull($groups['groups'][3][1]);
 	}
+
+	public function testCalculatesReasonableCost() {
+		// Sample data from: https://app.matchplay.events/tournaments/166935
+		// 1: Godzilla
+		// 2: Jaws
+		// 3: Mandalorian
+		// 4: Uncanny X-Men
+		// 5: Venom
+	    $groups = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+		$arenas = [1, 2, 3, 4, 5];
+		$amount = 1;
+		$plays = [
+			1 => [3 => 3],
+			2 => [1 => 1, 4 => 1, 5 => 1],
+			3 => [1 => 3],
+			4 => [1 => 1, 4 => 1, 5 => 1],
+			5 => [2 => 2, 4 => 1],
+			6 => [1 => 1, 2 => 1, 3 => 1],
+			7 => [4 => 1, 5 => 1, 3 => 1],
+			8 => [1 => 1, 3 => 1, 5 => 1],
+			9 => [4 => 1, 2 => 1, 1 => 1],
+			10 => [2 => 1, 5 => 2],
+			11 => [3 => 1, 1 => 1, 4 => 1],
+			12 => [1 => 1, 3 => 1, 2 => 1],
+			13 => [3 => 1, 5 => 1, 1 => 1],
+			14 => [3 => 1, 2 => 1, 5 => 1],
+			15 => [2 => 1, 1 => 1, 3 => 1],
+			16 => [4 => 2, 3 => 1],
+		];
+    	$builder = new haugstrup\TournamentUtils\BalancedArena($groups, $arenas, $amount, $plays);
+    	$groups = $builder->build();
+
+    	$this->assertEquals(count($groups['groups']), 4);
+    	$this->assertEquals($groups['cost'], 6);
+	}
 }
